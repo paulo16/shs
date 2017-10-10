d47ed02dae4c3050aa8ef78410813fc97189b97e
MMPaul <paul.mbilong@gmail.com>
2017-10-10 11:22:07 +0100

    cacher ou afficher l'historique des paiements

--- imports/ui/pages/adminTo/paiements/effectuerPaiement.js
+++ imports/ui/pages/adminTo/paiements/effectuerPaiement.js
@@ -59,107 +59,16 @@ Template.effectuerPaiement.events({
                 }
             });
 
-            Meteor.call('findHistorique', $('#input-search').val(), function (error, result1) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    //console.log(result);
-                    //let content = '<span> Aucun résulat trouvé</span>';
-                    let dataset = [];
-                    Session.set('result1', result1);
-                    let res1 = Session.get('result1');
-                    if (res1 && res1 != undefined && res1 != "") {
-
-                        let contenu;
-                        let lien;
-                        res1.forEach(function (element) {
-                            if (element.recu_pdf) {
-                                lien = element.recu_pdf.replace(/.*pdfs\\/, "");
-                            }
-                            contenu = [
-                                element.agent.lastName,
-                                element.client.cin,
-                                moment(element.date_paiement_auto).format("DD-MM-YYYY HH:mm"),
-                                //element.client.type_recu,
-                                lien,
-                                element.montant
-
-                            ];
-                            dataset.push(contenu);
-                        })
-
-
-                    }
-                    $("#table-historique").dataTable({
-                        data: dataset,
-                        destroy: true,
-                        pageLength: 3,
-                        "oLanguage": {
-                            "sProcessing": "Traitement en cours  ...",
-                            "sSearch": "Rechercher&nbsp;:",
-                            "sLengthMenu": "Afficher _MENU_ ",
-                            "sInfo": "Affichage de l' &eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-                            "sInfoEmpty": "Affichage de l\&eacute;l&eacute;ment0&agrave;0sur0&eacute;l&eacute;ments",
-                            "sInfoFiltered": "filtr&eacute; de _MAX_ &eacute; l&eacute;mentsautotal",
-                            "sInfoPostFix": "",
-                            "sLoadingRecords": "Chargement en cours...",
-                            "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
-                            "sEmptyTable": "Aucune donn&eacute;e disponible dans letableau",
-                            "oPaginate": {
-                                "sFirst": "Premier",
-                                "sPrevious": "Pr&eacute;c&eacute;dent",
-                                "sNext": "Suivant",
-                                "sLast": "Dernier"
-                            },
-                            "oAria": {
-                                "sSortAscending": "activer pour trier la colonne par ordre croissant",
-                                "sSortDescending": "activer pour trier la colonne par ordre d&eacute;croissant"
-                            }
-                        },
-                        columns: [
-                            { title: "Nom agent" },
-                            { title: "client" },
-                            { title: "Date Paiement" },
-                            { title: "Reçu" },
-                            //{ title: "Type Reçu" },
-                            { title: "Montant" },
-                        ]
-                    });
-                }
-            });
-
-            Meteor.call('SumPaiementParClient', $('#input-search').val(), (error, result2) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    if (result2.length > 0) {
-                        Session.set('result2', result2);
-                        let res2 = Session.get('result2');
-                        ////console.log(JSON.stringify(result));
-                        let element = '<div style="height: 70px;line-height: 70px;text-align: center;border: 1px dashed #f69c55;">';
-                        let scp = monthDiff(res2[0]._id.date_mise_service, new Date()) * 30;
-                        let msp = monthDiff(res2[0]._id.date_mise_service, new Date());
-                        let sp = res2[0].total ? res2[0].total : 0;
-                        let mp = Math.floor(sp / 30);
-                        let sa = Math.abs(scp - sp);
-                        let mpa = Math.abs(Math.floor(sa / 30));
-                        element += '<span>&nbsp;&nbsp;&nbsp;SOMME TOTAL PAYER (' + mp + ' mois):<b>&nbsp;' + sp + 'DH</b></span>';
-                        element += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>SOMME CENSE PAYER (' + msp + ' mois)<b>:&nbsp;' + scp + 'DH</b></span>';
-                        element += sp > scp ? '<span style="color:green;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SOMME PAYE AVANCE (' + mpa + ' mois) <b>:&nbsp;' + sa + 'DH </b></span>' : '<span style="color:red;">&nbsp;&nbsp;&nbsp;<b>SOMME RETARD (' + mpa + ' mois):&nbsp; ' + sa + 'DH</b></span>';
-                        element += '</div>'
-                        $('#les-montants').html(element);
-
-                    }
-                }
-
-            });
-
+            showhistorique();
 
         } else {
             $('#historique-paie').html('');
             $('#info-client').html('');
         }
+    },
+
+    'click #refresh': function (event, template) {
+        showhistorique();
     },
 
     'change #type-paie': function (event, template) {
@@ -324,3 +233,105 @@ function monthDiff(d1, d2) {
     months += d2.getMonth();
     return months <= 0 ? 0 : months;
 }
+
+
+function showhistorique() {
+    Meteor.call('findHistorique', $('#input-search').val(), function (error, result1) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            //console.log(result);
+            //let content = '<span> Aucun résulat trouvé</span>';
+            let dataset = [];
+            Session.set('result1', result1);
+            let res1 = Session.get('result1');
+            if (res1 && res1 != undefined && res1 != "") {
+
+                let contenu;
+                let lien;
+                res1.forEach(function (element) {
+                    if (element.recu_pdf) {
+                        lien = element.recu_pdf.replace(/.*pdfs\\/, "");
+                    }
+                    contenu = [
+                        element.agent.lastName,
+                        element.client.cin,
+                        moment(element.date_paiement_auto).format("DD-MM-YYYY HH:mm"),
+                        //element.client.type_recu,
+                        lien,
+                        element.montant
+
+                    ];
+                    dataset.push(contenu);
+                })
+
+
+            }
+            $("#table-historique").dataTable({
+                data: dataset,
+                destroy: true,
+                pageLength: 3,
+                dom: '<tip>',
+                "oLanguage": {
+                    "sProcessing": "Traitement en cours  ...",
+                    "sSearch": "Rechercher&nbsp;:",
+                    "sLengthMenu": "Afficher _MENU_ ",
+                    "sInfo": "Affichage de l' &eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+                    "sInfoEmpty": "Affichage de l\&eacute;l&eacute;ment0&agrave;0sur0&eacute;l&eacute;ments",
+                    "sInfoFiltered": "filtr&eacute; de _MAX_ &eacute; l&eacute;mentsautotal",
+                    "sInfoPostFix": "",
+                    "sLoadingRecords": "Chargement en cours...",
+                    "sZeroRecords": "Aucun &eacute;l&eacute;ment &agrave; afficher",
+                    "sEmptyTable": "Aucune donn&eacute;e disponible dans letableau",
+                    "oPaginate": {
+                        "sFirst": "Premier",
+                        "sPrevious": "Pr&eacute;c&eacute;dent",
+                        "sNext": "Suivant",
+                        "sLast": "Dernier"
+                    },
+                    "oAria": {
+                        "sSortAscending": "activer pour trier la colonne par ordre croissant",
+                        "sSortDescending": "activer pour trier la colonne par ordre d&eacute;croissant"
+                    }
+                },
+                columns: [
+                    { title: "Nom agent" },
+                    { title: "client" },
+                    { title: "Date Paiement" },
+                    { title: "Reçu" },
+                    //{ title: "Type Reçu" },
+                    { title: "Montant" },
+                ]
+            });
+        }
+    });
+
+    Meteor.call('SumPaiementParClient', $('#input-search').val(), (error, result2) => {
+        if (error) {
+            console.log(error);
+        } else {
+            if (result2.length > 0) {
+                Session.set('result2', result2);
+                let res2 = Session.get('result2');
+                ////console.log(JSON.stringify(result));
+                let element = '<div style="height: 70px;line-height: 70px;text-align: center;border: 1px dashed #f69c55;">';
+                let scp = monthDiff(res2[0]._id.date_mise_service, new Date()) * 30;
+                let msp = monthDiff(res2[0]._id.date_mise_service, new Date());
+                let sp = res2[0].total ? res2[0].total : 0;
+                let mp = Math.floor(sp / 30);
+                let sa = Math.abs(scp - sp);
+                let mpa = Math.abs(Math.floor(sa / 30));
+                element += '<span>&nbsp;&nbsp;&nbsp;SOMME TOTAL PAYER (' + mp + ' mois):<b>&nbsp;' + sp + 'DH</b></span>';
+                element += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>SOMME CENSE PAYER (' + msp + ' mois)<b>:&nbsp;' + scp + 'DH</b></span>';
+                element += sp > scp ? '<span style="color:green;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SOMME PAYE AVANCE (' + mpa + ' mois) <b>:&nbsp;' + sa + 'DH </b></span>' : '<span style="color:red;">&nbsp;&nbsp;&nbsp;<b>SOMME RETARD (' + mpa + ' mois):&nbsp; ' + sa + 'DH</b></span>';
+                element += '</div>'
+                $('#les-montants').html(element);
+
+            }
+        }
+
+    });
+
+}
+
